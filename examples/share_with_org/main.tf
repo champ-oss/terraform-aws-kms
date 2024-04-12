a2016c0b69d04953e590406f12d617e3d7ce02fd
Justin Reeves <reeves122@gmail.com>
2024-04-12 13:38:53 -0500

    Update main.tf

--- examples/share_with_org/main.tf
+++ examples/share_with_org/main.tf
@@ -3,7 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0"
     }
   }
 }
